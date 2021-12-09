fa6b48690c28e4a5df3aa5a1988ff1882ac9a4b1
ywkim <akr1253@naver.com>
2021-12-09 23:57:05 +0900

    Create sum_cases.py

---
+++ Algorithm/sum_cases.py
@@ -0,0 +1,25 @@
+num, =map(int, input().split())
+def pick(n, picked, to_pick=0):
+    global counts
+    if to_pick >= n:
+        if to_pick==n:
+            #ans = picked.copy()
+            print('+'.join([str(i) for i in picked]))
+            counts+=1
+            #result.append(ans)
+            return
+        else: return 
+    
+    if not picked:
+        smallest = n-1
+    else:
+        smallest = picked[-1]
+    
+    for _next in range(smallest, 0,-1):
+        picked.append(_next)
+        pick(n, picked, to_pick+_next)
+        picked.pop()
+        
+counts = 0
+pick(num, [], )
+print(counts)
